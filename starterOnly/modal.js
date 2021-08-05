a3f0a17aa703beecd355b44eb0b3f1ab35bc9ee3
Thomas <thomas.vandekerchove@outlook.com>
2021-08-05 12:45:29 +0200

    Fermeture de la modal principale et ajout de la seconde

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,4 +20,45 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//Close modal form
+modalClose.addEventListener('click', () => {
+  if(getComputedStyle(modalbg).display != "none"){
+      modalbg.style.display = "none";
+  }
+})
+
+
+
+// close second modal after submiting
+const secondModalClose = document.querySelector('.close2');
+const btnSecondModalClose = document.querySelector('btnAfterSubmit');
+
+secondModalClose.addEventListener('click', () => {
+  if(getComputedStyle(modalBg2, btnSecondModalClose).display != "none"){
+    modalBg2.style.display = "none";
+    btnSecondModalClose.style.display = "none";
+  }
+})
+
+
+// Contrôle de validation des champs
+let myForm = document.getElementsByName('reserve');
+
+// DOM elements form
+const monPrenom = document.getElementById('first');
+const monNom = document.getElementById('last');
+const monEmail = document.getElementById('email');
+const myRegex = /^[a-zA-Z-\s]+$/;
+
+myForm.addEventListener('submit', function (e) {
+
+  if (monPrenom.value.trim() == '') {
+    alert('Compléter les champs')
+  }
+})
+
+// launch second modal after submit registration ok
+const modalBg2 = document.querySelector('.bground2');
+const modalbgAfterSubmit = document.querySelector(".bground2");
+const modalBtnSubmit = document.querySelector(".modal-btn-submit");
 
