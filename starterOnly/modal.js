e1220d8b0a360cbae0d864b0ae04a9c1b26d2d2d
Thomas Vandekerchove <74045212+Thomasvdk783@users.noreply.github.com>
2021-08-23 08:25:34 +0200

    Update modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -41,194 +41,90 @@ const citysCheckboxInput = document.getElementsByClassName(".checkbox-input");
 const tabsLocation = document.forms.reserve.location;
 const birthdateInput = document.getElementById('birthdate');
 
+// Error Elements
+const firstNameError = document.getElementById("first-name-error");
+const lastNameError = document.getElementById("last-name-error");
+const emailError = document.getElementById("email-error");
+const quantityError = document.getElementById("quantity-error");
+const errorConditionsCheckbox = document.getElementById("error-checkbox1");
+const errorCityCheckbox = document.getElementById('city-checkbox-error');
+const birthdateError = document.getElementById('date-error')
 
+function validateInput(input, errorTag, regex, message) {
+    //first name error message
+    if (input.value.trim() == "") {
+        errorTag.innerHTML = "Le champ ci-dessus est requis";
+        errorTag.style.color = "red";
+        errorTag.style.fontSize = "14px";
+        errorTag.style.display = "flex";
+        errorTag.style.justifyContent = "flex-end";
+        return false
+    } else if (regex.test(input.value) == false) {
+        errorTag.innerHTML = message;
+        errorTag.style.color = "red";
+        errorTag.style.fontSize = "14px";
+        errorTag.style.display = "flex";
+        errorTag.style.justifyContent = "flex-end";
+        return false
+    } else {
+        errorTag.style.display = "none"
+        return true
+    }
+}
+
+function validateCheckbox(conditionToCheck, errorTag, message) {
+    if (conditionToCheck == false) {
+        errorTag.innerHTML = message;
+        errorTag.style.color = "red";
+        errorTag.style.fontSize = "14px";
+        errorTag.style.display = "flex";
+        errorTag.style.justifyContent = "flex-end";
+        return false;
+    } else {
+        errorTag.style.display = "none";
+        return true;
+    }
+}
 
 function validate(e) {
     e.preventDefault();
 
-    // Error Elements
-    const firstNameError = document.getElementById("first-name-error");
-    const lastNameError = document.getElementById("last-name-error");
-    const emailError = document.getElementById("email-error");
-    const quantityError = document.getElementById("quantity-error");
-    const errorConditionsCheckbox = document.getElementById("error-checkbox1");
-    const errorCityCheckbox = document.getElementById('city-checkbox-error');
-    const birthdateError = document.getElementById('date-error');
-
-
-    // REGEX
-    const nameRegex = /^[a-zA-Z-\s]+$/;
-    const emailRegex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
-    const quantityRegex = /^[0-9]+$/;
-    const dateRegex = /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/;
-
-    //first name error message
-    if (firstNameInput.value.trim() == "") {
-        firstNameError.innerHTML = "Le champ ci-dessus est requis";
-        firstNameError.style.color = "red";
-        firstNameError.style.fontSize = "14px";
-        firstNameError.style.display = "flex";
-        firstNameError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else if (nameRegex.test(firstNameInput.value) == false) {
-        firstNameError.innerHTML = "Le champ doit comporter uniquement des lettres, des tirets.";
-        firstNameError.style.color = "red";
-        firstNameError.style.fontSize = "14px";
-        firstNameError.style.display = "flex";
-        firstNameError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else {
-        isFormValid = true;
-        firstNameError.style.display = "none"
-    }
-    //Last name error message
-    if (lastNameInput.value.trim() == "") {
-        lastNameError.innerHTML = "Le champ ci-dessus est requis";
-        lastNameError.style.color = "red";
-        lastNameError.style.fontSize = "14px";
-        lastNameError.style.display = "flex";
-        lastNameError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else if (nameRegex.test(lastNameInput.value) == false) {
-        lastNameError.innerHTML = "Le champ doit comporter uniquement des lettres, des tirets.";
-        lastNameError.style.color = "red";
-        lastNameError.style.fontSize = "14px";
-        lastNameError.style.display = "flex";
-        lastNameError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else {
-        isFormValid = true;
-        lastNameError.style.display = "none"
-    }
-    // Email Error message
-    if (emailInput.value.trim() == "") {
-        emailError.innerHTML = "Le champ ci-dessus est requis";
-        emailError.style.color = "red";
-        emailError.style.fontSize = "14px";
-        emailError.style.display = "flex";
-        emailError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else if (emailRegex.test(emailInput.value) == false) {
-        emailError.innerHTML = "Veuillez vérifier que l'email est valide";
-        emailError.style.color = "red";
-        emailError.style.fontSize = "14px";
-        emailError.style.display = "flex";
-        emailError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else {
-        isFormValid = true;
-        emailError.style.display = "none"
-        
-    }
-
-    // Birthdate Check
-    if (birthdateInput.value.trim() == "") {
-        birthdateError.innerHTML = "Vous devez sélectionner une date"
-        birthdateError.style.color = "red";
-        birthdateError.style.fontSize = "14px";
-        birthdateError.style.display = "flex";
-        birthdateError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else if (dateRegex.test(birthdateInput.value) == false) {
-        birthdateError.innerHTML = "La date est incorrecte";
-        birthdateError.style.color = "red";
-        birthdateError.style.fontSize = "14px";
-        birthdateError.style.display = "flex";
-        birthdateError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else {
-        isFormValid = true;
-        birthdateError.style.display = "none"
-    }
-
-    // Quantity error message
-    if (quantityInput.value.trim() == "") {
-        quantityError.innerHTML = "Le champ ci-dessus est requis";
-        quantityError.style.color = "red";
-        quantityError.style.fontSize = "14px";
-        quantityError.style.display = "flex";
-        quantityError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else if (quantityRegex.test(quantityInput.value) == false) {
-        quantityError.innerHTML = "Seulement des caractères numérique sont autorisés";
-        quantityError.style.color = "red";
-        quantityError.style.fontSize = "14px";
-        quantityError.style.display = "flex";
-        quantityError.style.justifyContent = "flex-end";
-        isFormValid = false;
-    } else {
-        isFormValid = true;
-        quantityError.style.display = "none"
-    }
-
     // Selection of city
     let isLocationChecked = false;
-    // tabsLocation.forEach(radio => {
-    //     if (radio.checked == true) { isLocationChecked = true }
-    // });
-
-    // for (const radio of tabsLocation) {
-    //     if (radio.checked == true) { isLocationChecked = true }
-    // }
-
     for (let index = 0; index < tabsLocation.length; index++) {
         const radio = tabsLocation[index];
         if (radio.checked == true) { isLocationChecked = true }
     }
 
-    if (isLocationChecked == false) {
-        errorCityCheckbox.innerHTML = "Vous devez choisir une ville";
-        errorCityCheckbox.style.color = "red";
-        errorCityCheckbox.style.fontSize = "14px";
-        errorCityCheckbox.style.display = "flex";
-        errorCityCheckbox.style.justifyContent = "flex-end";
-        e.preventDefault();
-    } else {
-        errorCityCheckbox.style.display = "none";
-    }
+    // REGEX
+    const nameRegex = /^[a-zA-Z]+[a-zA-Z-]?[a-zA-Z]+$/;
+    const emailRegex = /^[a-z0-9]+([_|\.|-]{1}[a-z0-9]+)*@[a-z0-9]+([_|\.|-]­{1}[a-z0-9]+)*[\.]{1}[a-z]{2,6}$/;
+    const quantityRegex = /^[0-9][0-9]?$/;
+    const dateRegex = /^(19|20)\d\d[-](0[1-9]|1[012])[-](0[1-9]|[12][0-9]|3[01])$/;
 
-    // Conditions Checkbox error
-    if (conditionsCheckboxInput.checked == false) {
-        errorConditionsCheckbox.innerHTML = "Vous devez accepter les conditions utilisations";
-        errorConditionsCheckbox.style.color = "red";
-        errorConditionsCheckbox.style.fontSize = "14px";
-        errorConditionsCheckbox.style.display = "flex";
-        errorConditionsCheckbox.style.justifyContent = "flex-end";
-        e.preventDefault();
-    } else {
-        errorConditionsCheckbox.style.display = "none";
-    }
-    e.preventDefault()
+    const isFirstNameValid = validateInput(firstNameInput, firstNameError, nameRegex, "Le champ doit comporter uniquement des lettres, des tirets.");
+    const isLastNameValid = validateInput(lastNameInput, lastNameError, nameRegex, "Le champ doit comporter uniquement des lettres, des tirets.");
+    const isEmailValid = validateInput(emailInput, emailError, emailRegex, "Veuillez vérifier que l'email est valide");
+    const isBirthdayValid = validateInput(birthdateInput, birthdateError, dateRegex, "La date est incorrecte");
+    const isQuantityValid = validateInput(quantityInput, quantityError, quantityRegex, "Seulement des caractères numérique sont autorisés");
+    const isLocationValid = validateCheckbox(isLocationChecked, errorCityCheckbox, "Vous devez choisir une ville")
+    const isConditionValid = validateCheckbox(conditionsCheckboxInput.checked, errorConditionsCheckbox, "Vous devez accepter les conditions utilisations")
 
-    if (isFormValid) {
+    if (isFirstNameValid && isLastNameValid && isEmailValid && isBirthdayValid && isQuantityValid && isLocationValid && isConditionValid) {
+        console.log("form valid");
         showConfirmationModal(modalBg2, modalbg);
     }
-
 }
 
 function showConfirmationModal(modalBg2, modalbg) {
-    if (isFormValid === true) {
-        modalBg2.style.display = "block"
-        modalbg.style.display = "none";
-    }
-    if (lastNameInput.value == true) {
-        modalBg2.style.display != "block"
-    }
-    if (emailInput.value == true) {
-        modalBg2.style.display = "block"
-    }
+    modalBg2.style.display = "block"
+    modalbg.style.display = "none";
 }
-
-
-
-
 
 // launch second modal after submit registration ok
 const modalBtnSubmit = document.querySelector("submitbtn");
 const modalBg2 = document.getElementById('second-modal');
 const modalbgAfterSubmit = document.querySelector(".bground2-close");
-
-
 
 // close second modal after submiting
 const secondModalClose = document.querySelector('.close2');
